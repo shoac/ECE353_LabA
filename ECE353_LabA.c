51a14a6ab8e09d2121de59afadf3065edfc65f5c
uma-donl <dliang@umass.edu>
2014-09-03 21:03:07 -0400

    Create ECE353_LabA.c
    Template of required functions. Fill in with code.

---
+++ ECE353_LabA.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+main(){
+}
+
+int whichSet(){
+}
+
+int setIndexLength(){
+}
+
+int offsetLength(){
+}
+
+int tagBits(){
+}
+
+int hitWay(){
+}
+
+void updateOnHit(){
+}
+
+void updateOnMiss(){
+}
