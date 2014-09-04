abb26c15d2e553ea36b80a7e5ce16fc21f4c3025
uma-donl <dliang@umass.edu>
2014-09-03 21:22:27 -0400

    Update ECE353_LabA.c
    how to find offset length

--- ECE353_LabA.c
+++ ECE353_LabA.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
-//input: L(line length), K(number of lines per set), C(cache size)
+//input: L(line length/block size), K(number of lines per set), C(cache size)
+//assuming byte addressable
 
 main(){
   
@@ -16,7 +17,7 @@ int setIndexLength(){
 }
 
 int offsetLength(){
-  
+  //log2(L), log2 of the block size is the number of bits required for the offset
 }
 
 int tagBits(){
