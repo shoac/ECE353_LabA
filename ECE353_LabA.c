f75c07bfaa7646222103036ac5070fff8e421918
uma-donl <dliang@umass.edu>
2014-09-03 21:13:26 -0400

    Update ECE353_LabA.c

--- ECE353_LabA.c
+++ ECE353_LabA.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 #include <math.h>
 
+//input: L(line length), K(number of lines per set), C(cache size)
+
 main(){
+  
 }
 
 int whichSet(){
+
 }
 
 int setIndexLength(){
+  
 }
 
 int offsetLength(){
+  
 }
 
 int tagBits(){
+  
 }
 
 int hitWay(){
+  
 }
 
 void updateOnHit(){
+  
 }
 
 void updateOnMiss(){
+  
 }
