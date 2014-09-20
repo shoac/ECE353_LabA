fe44d6bd62445256fe234cc095e9072891a20948
shoac <shoac@umass.edu>
2014-09-19 21:50:44 -0400

    Update ECE353_LabA.c

--- ECE353_LabA.c
+++ ECE353_LabA.c
@@ -1,89 +1,170 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
 
-//input: L(line length or block size), K(number of lines per set), C(cache size)
-//assuming byte addressable
+int **tagArray;
+int **lruArray;
+int rows; //# of rows of cache array
+int columns;//# of columns in cache array
 
-main(){
-  unsigned int **tagArray; //
-  int **lruArray;
-  int rows;
-  int columns;
-  
-  tagArray = (int**) malloc(rows*sizeof(int *)); //allocates memory that can hold i*sizeof(int*) bytes for tagArray
-  lruArray = (int**) malloc(rows*sizeof(int *)); //allocates memory that can hold i*sizeof(int*) bytes for lruArray
-  
-  FILE *ifp; //declare pointer to file
-  ifp = fopen("trace.txt","r"); //*ifp points to trace.txt, open for read
-  while (fscanf(ifp,"%d %x",&t) != EOF){ //assuming decimal, reads decimal and hex
-    //reads each access in the file(t), perform required actions here
-  }
+int logTwo(int num){//function to solve log2 of a # 
+	int i = 0;
+	while (num > 1){
+		num = num / 2;
+		i++;
+	}
+	return i;
+} 
+
+int twoToThePowerOf(int num){//function to for computing 2^n
+	int i;
+	int twos = 1;
+	for (i = 0; i < num; i++)
+		twos *= 2;
+
+	return twos;
 }
 
-int whichSet(int fullAddress){    //Outputs the cache set in which the address falls.
-  unsigned int a = fullAddress;   //change full address into unsigned integer
-  int numTagBit = 32 - setIndexLength(L,K,C) - offsetLength(L); //calculate the number of tag bit
-  a<<numTagBit;                   //get rid of tag bits, left with index|offset|0's
-  a>>(numTagBit+offsetLength(L)); //get rid of offset bits and trailing 0's, leaves index
-  return a;                       //return set index
-  
+int offsetLength(int sizeOfLine){//returns # of bits in offset
+	return logTwo(sizeOfLine);
 }
 
-int setIndexLength(int numberOfLines, int sizeOfLine, int cacheSize){
-  // Outputs the number of bits in the set index field.
-  //set size = K*L [number of lines/set * size of each line]
-  //# of sets = C/(K*L) [total cache size/set size = number of sets]
-  //log2(# of sets)
-  return log2(cacheSize/(numberOfLines*sizeOfLine));
+int setIndexLength(int numberOfLinesPerSet, int sizeOfLine, int cacheSize){//returns # of bits in index
+	int setSize = numberOfLinesPerSet * sizeOfLine;
+	int numberOfSets = cacheSize / setSize;
+	return logTwo(numberOfSets);
 }
 
-int offsetLength(int sizeOfLine){
-  //log2(L), log2 of the block size is the number of bits required for the offset
-  return log2(L);
+int tagBits(int fullAddress, int numberOfLinesPerSet, int sizeOfLine, int cacheSize){//returns  # of tag bits
+	unsigned int address = fullAddress;
+	return address >> (setIndexLength(numberOfLinesPerSet, sizeOfLine, cacheSize) + offsetLength(sizeOfLine)); //push out index and offset bits
 }
 
-int tagBits(int fullAddress){
-  //Outputs the tag bits associated with the address.
-  //# of tagBits = Address length - offsetLength - setIndexLength
-  //address is unsigned int
-  //shift right but offsetLength + setIndexLength to isolate tag bits
-  unsigned int a = fullAddress;   //change full address into unsigned integer
-  a>>(setIndexLength(L,K,C) + offsetLength(L));             //get rid of set index and offset bits
-  return a;
+int whichSet(int fullAddress, int numberOfLinesPerSet, int sizeOfLine, int cacheSize){//outputs cache set in which the address falls
+	unsigned int address = fullAddress;
+	int numberOfTagBits = 32 - setIndexLength(numberOfLinesPerSet, sizeOfLine, cacheSize) - offsetLength(sizeOfLine);
+	address = address << numberOfTagBits; //push out tag bits, left with index|offset|trailing 0's
+	return address >> (numberOfTagBits + offsetLength(sizeOfLine)); //push out offset bits and trailing 0's
 }
 
-
-int hitWay(int fullAddress,int numberOfLines){
-  //if hit, return cache way
-  //if miss, return -1
-  //if hit
-  for(int j = 0;j<numberOfLines;j++){
-     if((tagArray[whichSet(fullAddress)][j] == tagBits(fullAddress)) && (lruArray[whichSet(fullAddress)][j] > -1))
-       return j;            //if tag = tag && lru != -1 then return cache way(j)
-    else return -1;
-  }
+int hitWay(int numberOfLinesPerSet, int set, int tag){//indicates whether the access was a hit or a muss
+	int whichWay; 
+	for (whichWay = 0; whichWay<numberOfLinesPerSet; whichWay++){
+		if (tagArray[set][whichWay] == tag){
+			return whichWay; //hit
+		}
+		else{
+			return -1; //miss
+		}
+	}
 }
 
-void updateOnHit(){
-  int i;
-  int j;
-  for(i=0; i<rows; i++){
-    for(j=0; j<columns; j++){
-      if lruArray[i][j]!=-1;  //if element in lruArray is valid
-        lruArray[i][j]++; //increase valid line age by 1
-    }
-  }
+void updateOnHit(int fullAddress, int columns, int set, int tag){
+	int cacheSet = set; //sets set as the cache set in which the address falls
+	int j;
+	for (j = 0; j<rows; j++){//updates age required elements
+		if (lruArray[cacheSet][j] != -1){//finds element in lruArray that is valid
+			lruArray[cacheSet][j]++; //increase valid line age by 1
+		}
+	}
+	lruArray[cacheSet][hitWay(fullAddress, columns, set, tag)] = 0; //sets age of valid line to 0(most recently accessed)
 }
 
-void updateOnMiss(){
-  int i;
-  int j;
-  for(i=0; i<rows; i++){
-    for(j=0; j<columns; j++){
-      if lruArray[i][j]!=-1;  //if element in lruArray is valid
-        lruArray[i][j]++; //increase valid line age by 1
-    }
-  }
-      //all validLineAge++ in lruArray
-  //replace LRU line(greatest valid line age in lruArray) in tagArray with new tag
+void updateOnMiss(int fullAddress, int columns, int set, int tag){
+	int cacheSet = set; //sets as the cache set in which the address falls
+	int j;
+	int LRU = 0; //variable for oldest valid line age(least recently used line)
+	int LRUj = 0; //variable for the least recently used line
+	for (j = 0; j<columns; j++){
+		if (lruArray[cacheSet][j]>LRU){ //finds an older valid line age in the set
+			LRU = lruArray[cacheSet][j]; //replaces current oldest valid line age with older age
+			LRUj = j; //line for the LRU line is updated
+		}
+		if (lruArray[cacheSet][j] != -1){ //if element in lruArray is valid
+			lruArray[cacheSet][j]++; //increase valid line age by 1
+		}
+	}
+	lruArray[cacheSet][LRUj] = 0; //sets age of valid line to 0(most recently accessed)
+	tagArray[cacheSet][LRUj] = tag;//updates tag for LRU line in the set
+}
+
+int main(){
+	//running assertions to make sure functions work
+	assert(offsetLength(16) == 4); //the offset field must contain 4-bits to accomodate a 16byte cache block size
+	//A 2-way with a capacity of 32kb, and a cache block contains 16bytes
+	assert(setIndexLength(2, 16, 32 * 1024) == 10); //10-bits for the index
+	//The address 0xFFFFCFF0 = 0d4294954992 should have a tag of all 1's, a set of 00 1111 1111, and a offset of 0
+	assert(tagBits(4294954992, 2, 16, 32 * 1024) == 262143);
+	assert(whichSet(4294954992, 2, 16, 32 * 1024) == 255);
+
+	int input;
+
+	int lineLength;
+	int linesPerSet;
+	long cacheSize;
+
+	//read inputs for L,K, and C
+	printf("Please enter the line length\n");
+	scanf("%d", &lineLength);
+	printf("Please enter the number of lines per set\n");
+	scanf("%d", &linesPerSet);
+	printf("Please enter the total cache size in kilobytes\n");
+	scanf("%d", &cacheSize);
+	cacheSize = cacheSize * 1024; //convert to bytes
+
+	int numberOfOffsetBits = offsetLength(lineLength);
+	int numberOfIndexBits = setIndexLength(linesPerSet, lineLength, cacheSize);
+
+	rows = twoToThePowerOf(numberOfIndexBits); //calculates the # of rows needed for the cache
+	columns = twoToThePowerOf(numberOfOffsetBits); //calculates the # of columns needed for the cache
+
+
+	int i;
+
+	tagArray = (int **) malloc(rows*sizeof(int)); //allocates memory that can hold rows*sizeof(int) bytes for tagArray
+	for (i=0; i<rows; i++){
+		*(tagArray+i) = (int **) malloc(columns*sizeof(int)); //pointer for the rows points to memory with columns
+		lruArray = (int **) malloc(rows*sizeof(int)); //allocates memory that can hold rows*sizeof(int) bytes for lruArray
+	}
+	for (i=0; i<rows; i++){
+		*(lruArray+i) = (int **) malloc(columns*sizeof(int)); //pointer for the rows points to memory with columns
+	}
+
+	double misses = 0;
+	double accesses = 0;
+	double missRate = 0;
+
+	FILE *ifp;      //declare standard pointer to file
+	ifp = fopen("trace.txt", "r");   //*ifp points to trace.txt, it is open for read
+	if (ifp == NULL) //checks if the file doesn't exist
+	{
+		perror("Error in opening file");  //prints the error 
+		return(-1);
+	}
+	while (!feof(ifp)){ //checks if the end of the file is reached
+		fscanf(ifp, "%d", &input); //reads in a line of the file and stores into variable input
+		printf("Address Read = %u\n", input);
+		int set = whichSet(input, linesPerSet, lineLength, cacheSize);
+		int tag = tagBits(input, linesPerSet, lineLength, cacheSize);
+                
+                //determines if the access to the address in the trace is a miss or a hit and updates the cache arrays accordingly
+		if (hitWay(linesPerSet, set, tag) == -1) {
+			updateOnMiss(input, columns, set, tag);
+			misses++;
+			accesses++;
+		}
+		else{
+			updateOnHit(input, columns, set, tag);  
+			accesses++;
+		}
+	}
+
+	missRate = misses / accesses * 100;//this is for the plots
+	free(tagArray); //deallocates memory previously allocated for tagArray
+	free(lruArray); //deallocates memory previously allocated for lruArray
+	printf("Miss Rate = %f%%\n", missRate); //outputs miss rate as a percentage
+
+	fclose(ifp);
 }
